1f7470142c68ded5c3b03cf34cbb35eb18537963
Dima Akzhigitov <trololoshka609@gmail.com>
2020-03-08 17:41:29 +0300

    Lab2 is ready

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -22,6 +22,23 @@ MainWindow::MainWindow(QWidget *parent)
     ui->btnLoadTable->setDisabled(true);
     ui->btnCountMetrics->setDisabled(true);
     QChart *chart = new QChart();
+    QLineSeries *series = new QLineSeries();
+    series->append(0, 6);
+    series->append(2, 4);
+    series->append(3, 8);
+    series->append(7, 4);
+    series->append(10, 5);
+    *series << QPointF(11, 1) << QPointF(13, 3) << QPointF(17, 6) << QPointF(18, 3) << QPointF(20, 2);
+    chart->legend()->hide();
+    chart->addSeries(series);
+    chart->createDefaultAxes();
+    chart->setTitle("Simple line chart example");
+    QChartView *chartView = new QChartView(chart);
+    chartView->setRenderHint(QPainter::Antialiasing);
+    QMainWindow window;
+    window.setCentralWidget(chartView);
+    window.resize(400, 300);
+//    window.show();
 }
 
 MainWindow::~MainWindow()
